268b8dceca1bdbe5dab97005d8aee9aaf35332dc
Humberto.perera <103610708+btopf17@users.noreply.github.com>
2023-06-01 13:13:52 -0600

    Create codigo unico.R
    nuevo codigo unico

---
+++ codigo unico.R
@@ -0,0 +1,49 @@
+#Instalacion de librerias
+# Instalación de paquetes
+install.packages("data.table")
+install.packages("RODBC")
+install.packages("ggplot2")
+install.packages("tidyverse")
+install.packages("summarytools")
+install.packages("GGally")
+install.packages("DT")
+install.packages("lubridate")
+install.packages("venn")
+install.packages("readxl")
+install.packages("stringi")
+install.packages("stringr")
+install.packages("writexl")
+install.packages("matrixStats")
+install.packages("robustbase")
+install.packages("rrcov")
+install.packages("factoextra")
+install.packages("h2o")
+install.packages("DDoutlier")
+install.packages("isotree")
+
+
+#Convocar las librerias
+
+# Cargando las librerias 
+library(data.table)
+library(RODBC)
+library(ggplot2)
+library(tidyverse)
+library(summarytools)
+library(GGally)
+library(DT)
+library(lubridate)
+library(venn)
+library(readxl)
+library(stringi)
+library(stringr)
+library(writexl)
+library(matrixStats)
+library(robustbase)
+library(rrcov)
+library(factoextra)
+library(h2o)
+library(DDoutlier)
+library(isotree)
+
+
