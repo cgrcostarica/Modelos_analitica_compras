15a1f45c351039b372d9162a1aaec655f28c52c4
Humberto.perera <103610708+btopf17@users.noreply.github.com>
2023-07-27 19:30:59 -0600

    cambio archivo bat
    Se modifica el archivo bat en la creacion y modificacion de la carpeta

--- Script_automatizacion.bat
+++ Script_automatizacion.bat
@@ -1,7 +1,16 @@
-@echo off 
-rmdir /s /q C:\Users\usuario\Documents\Codigo_compras\Modelos_analitica_compras
-cd C:\Users\usuario\Documents\Codigo_compras\
-git clone https://github.com/cgrcr/Modelos_analitica_compras
-"C:\Program Files\R\R-4.3.1\bin\Rscript.exe" C:\Users\usuario\Documents\Codigo_compras\Modelos_analitica_compras\CODIGO_UNICO_MODELO_2022_V2_Jun.R
-pause
+﻿@echo off 
+set "ruta=C:\Users\humberto.perera\Desktop\modelo_compras"
 
+rem Asegurarse de que la ruta exista antes de eliminarla
+if exist "%ruta%" (
+    rmdir /s /q "%ruta%"
+)
+
+rem Clonar el repositorio de Git solo si no se encontraron problemas al eliminar la ruta
+if not exist "%ruta%" (
+    git clone https://github.com/cgrcr/Modelos_analitica_compras "%ruta%"
+)
+
+rem Asegurarse de que Rscript esté instalado y la ruta sea correcta
+"C:\Program Files\R\R-4.3.1\bin\Rscript.exe" "%ruta%\CODIGO_UNICO_MODELO_2022_V4_Jul.R"
+pause
